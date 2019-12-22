e28fcf53141c4a0dd9327fe0ad37ab52aa6837be
Nick-infinity <nikhilg.me@gmail.com>
2019-12-23 03:37:41 +0530

    SinglyLinkedList: Code refactored & creating ll
    Signed-off-by: Nick-infinity <nikhilg.me@gmail.com>

--- LinkedList/linked_list.c
+++ LinkedList/linked_list.c
@@ -1,49 +1,87 @@
+/*
+*  C program for multiple functions of Singly linked list
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 
-struct Node
+// structure of node
+struct node 
 {
-    int data;
-    struct Node *next;
-};
+    int data; //data
+    struct node *next; //address
+}*head;
 
-// TRAVERSING & PRINTING THE LIST
-void traverse(struct Node *head)
-{
-    struct Node *temp = head;
-    while(temp != NULL)
-    {
-     printf("%d ",temp->data);
-     temp = temp->next;
-    }
-}
-
-// FINDING LENGTH OF THE LIST
-void ListLength(struct Node* head)
-{
-    struct  Node *temp = head;
-    int count = 0;
-    while(temp != NULL){
-        count++;
-        temp = temp->next;
-    }
-    printf("\nList have %d elements.",count);
-}
+/*
+* Functions for linked list
+*/
+void createList(int n); 
+void display();
 
 int main()
 {
-    // malloc is lenghty as compared to new in cpp
-    struct Node *head = (struct Node*)malloc(sizeof(struct Node));
-    struct Node *second = (struct Node*)malloc(sizeof(struct Node));
-    struct Node *third = (struct Node*)malloc(sizeof(struct Node));
+    int n;
+    printf("Enter the number of nodes to create: ");
+    scanf("%d",&n);
+    createList(n);
+    printf("\nEntire linked list data: ");
+    display();
+}
 
-    head->data = 1;
-    head->next = second;
-    second->data =2;
-    second->next = third;
-    third->data=3;
-    third->next=NULL;
-    traverse(head);
-    ListLength(head);
-    return 0;
+// Creating a linked list
+void createList(int n)
+{
+    struct node *tmp , *newNode;
+    int data;
+    head = (struct node*)malloc(sizeof(struct node));
+
+    //terminate if memory not allocated
+    if(head == NULL)
+    {
+        printf("\nUnable to allocate memory to head!");
+        exit (0);
+    }
+
+    // creating node 1 
+    printf("\nEnter the data of node 1: ");
+    scanf("%d",&data);
+    head->data = data;
+    head->next=NULL;
+    tmp = head;
+
+    // creating remaining n-1 nodes
+    for(int i =2 ; i<=n ; i++)
+    {
+        newNode = (struct node*)malloc(sizeof(struct node));
+        if(newNode == NULL)
+        {
+            printf("\nUnable to allocate memory to %d node!",i);
+            break;
+        }
+        printf("\nEnter the data of node %d: ",i);
+        scanf("%d",&data);
+        newNode->data = data;
+        newNode->next=NULL; 
+        tmp->next = newNode;      // link new node with previous node
+        tmp = tmp->next;         // update tmp & set to new node
+
+    }
+    
+}
+
+// Display entire list
+void display()
+{
+    if(head == NULL)
+    {
+        printf("\nList is empty!");
+        exit (0);
+    }
+    struct node *tmp = head;
+    while(tmp != NULL)
+    {
+        printf("\nData: %d",tmp->data); // print data of current node
+        tmp = tmp->next; // move to next  node
+    }
+
 }
